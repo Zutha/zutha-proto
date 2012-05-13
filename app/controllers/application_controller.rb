7e2bf6c01da1ba0074ecaba50404c052c9eae6f6
Chris Barnett <barney.chrisj@gmail.com>
2012-05-13 15:38:36 -0700

    added authorization for modifying items/users. Changed some views to haml.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :admin?
+
+	protected
+
+	def authorize
+		unless admin?
+			flash[:error] = "Unauthorized Access"
+			redirect_to root_path
+			false
+		end
+	end
+
+  def admin?
+  	current_user.admin
+  end
 end
