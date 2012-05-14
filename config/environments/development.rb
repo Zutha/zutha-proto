94b31f889bb369073046c60eba72f02d12e79742
Chris Barnett <barney.chrisj@gmail.com>
2012-05-13 18:00:55 -0700

    disabled asset debug to prevent duplicate alert on :confirm

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,6 @@ ZuthaProto::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
 end
